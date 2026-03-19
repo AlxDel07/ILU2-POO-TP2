f23d64a2ad4113c5a016f4cff9930ede74545bb1
AlxDel07 <alexandre.delarue@utoulouse.fr>
2026-03-19 19:17:19 +0100

    Prendre Etal

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -27,7 +27,11 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					System.out.println("Bienvenue villageois " + nomVisiteur);
+					StringBuilder questionForce = new StringBuilder();
+					questionForce.append("Quelle est votre force ?\n");
+					int forceUtilisateur = Clavier.entrerEntier(questionForce.toString());
+					controlEmmenager.ajouterGaulois(nomVisiteur,forceUtilisateur);
 					break;
 
 				default:
@@ -40,6 +44,19 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		StringBuilder bienvenue = new StringBuilder();
+		bienvenue.append("Bienvenue " + nomVisiteur + "\n");
+		bienvenue.append("Quelle est votre force \n");
+		int forceDruide = Clavier.entrerEntier(bienvenue.toString());
+		int effetPotionMax = -1;
+		int effetPotionMin = 0;
+		while(effetPotionMax < effetPotionMin) {
+			effetPotionMin = Clavier.entrerEntier("Quelle est la force de potion la plus faible que vous produisez ?");
+			effetPotionMax = Clavier.entrerEntier("Quelle est la force de potion la plus forte que vous produisez ?");
+			if(effetPotionMin > effetPotionMax) {
+				System.out.println("Attention Druide, vous vous êtes trompé entre minimum et maximum");
+			}
+		}
+		controlEmmenager.ajouterDruide(nomVisiteur, forceDruide, effetPotionMin, effetPotionMax);
 	}
 }
