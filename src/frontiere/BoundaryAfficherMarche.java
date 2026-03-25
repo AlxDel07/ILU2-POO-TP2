7c0a6e28b8e677c5a757790475aef2476c72490a
Alexandre Delarue <alexandre.delarue@utoulouse.fr>
2026-03-25 09:13:55 +0100

    Afficher Marche

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,20 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		if(infosMarche.length == 0) {
+			System.out.println("Le marché est vide revenez plus tard");
+		}else {
+			System.out.println(nomAcheteur + " vous trouverez au marché :");
+			for(int i=0;i<infosMarche.length;i++) {
+				StringBuilder infos = new StringBuilder();
+				infos.append("- " + infosMarche[i]);
+				i++;
+				infos.append(" qui vend " + infosMarche[i]);
+				i++;
+				infos.append(" " + infosMarche[i]);
+				System.out.println(infos.toString());
+			}	
+		}
 	}
 }
